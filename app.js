20eb5fc4dddd7fd286240986606f376c64f6b2fa
paulrivera1994 <100796671+paulrivera1994@users.noreply.github.com>
2023-01-04 13:31:00 -0700

    finished exercise

--- app.js
+++ app.js
@@ -2,3 +2,97 @@ console.log("Hello World!\n==========\n");
 console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
+// Exercise 1
+let favNum = 7;
+let answer = prompt("input number");
+if (answer < favNum) {
+  console.log("too low");
+} else if (answer > favNum) {
+  console.log("too high");
+} else {
+  console.log("Congratulations!!!");
+}
+// Exercise 2
+let birthMonth = prompt("What is your birth month?");
+switch (birthMonth) {
+  case "December":
+  case "January":
+  case "February":
+    console.log("Winter");
+    break;
+  case "March":
+  case "April":
+  case "May":
+    console.log("Spring");
+    break;
+  case "June":
+  case "July":
+  case "August":
+    console.log("Summer");
+    break;
+  case "September":
+  case "October":
+  case "November":
+    console.log("Fall");
+  default:
+    console.log("Invalid Month");
+}
+// Exercise 3
+let typeId = "01";
+let colorId = "PU";
+let sizeId = "L";
+
+let type = "";
+let color = "";
+let size = "";
+
+switch (typeId) {
+  case "01":
+    type = "Tank Top";
+    break;
+  case "02":
+    type = "T-shirt";
+    break;
+  case "03":
+    type = "Long Sleeve";
+    break;
+  case "04":
+    type = "Sweat Shirt";
+    break;
+  default:
+    type = "Other";
+}
+switch (colorId) {
+  case "BK":
+    color = "Black";
+    break;
+  case "BL":
+    color = "Blue";
+    break;
+  case "RD":
+    color = "Red";
+    break;
+  case "PU":
+    color = "Purple";
+    break;
+  default:
+    color = "White";
+}
+switch (sizeId) {
+  case "S":
+    size = "Small";
+    break;
+  case "M":
+    size = "Medium";
+    break;
+  case "L":
+    size = "Large";
+    break;
+  case "XL":
+    size = "Extra Large";
+    break;
+  default:
+    size = "One size fits all";
+}
+
+console.log(`Product: ${size} ${color} ${type}`);
